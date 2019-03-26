b72e21414009feee6cb613dd7496d160e5af5668
Sherwood Richers <srichers@caltech.edu>
2019-03-26 17:40:35 -0400

    fix license

--- headers/misc.h
+++ headers/misc.h
@@ -1,27 +1,20 @@
 /*
-//  Copyright (c) 2015, California Institute of Technology and the Regents
-//  of the University of California, based on research sponsored by the
-//  United States Department of Energy. All rights reserved.
+//  Copyright (c) 2018, James Kneller and Sherwood Richers
 //
-//  This file is part of Sedonu.
+//  This file is part of IsotropicSQA.
 //
-//  Sedonu is free software: you can redistribute it and/or modify
+//  IsotropicSQA is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
-//  Neither the name of the California Institute of Technology (Caltech)
-//  nor the University of California nor the names of its contributors 
-//  may be used to endorse or promote products derived from this software
-//  without specific prior written permission.
-//
-//  Sedonu is distributed in the hope that it will be useful,
+//  IsotropicSQA is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with Sedonu.  If not, see <http://www.gnu.org/licenses/>.
+//  along with IsotropicSQA.  If not, see <http://www.gnu.org/licenses/>.
 //
 */
 
